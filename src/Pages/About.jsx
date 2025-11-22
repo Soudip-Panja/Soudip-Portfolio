a747a63faa9b4413779ed63c8fa8b5183f4175b8
Soudip Panja <soudip03panja@gmail.com>
2025-11-22 18:40:10 +0530

    feat: About section header part.

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,8 +1,21 @@
+import { Sparkles } from "lucide-react";
+
 export default function About() {
   return (
     <>
-      <section id="about" className="container">
-        <h1>About Me</h1>
+      <section id="about" className="container py-5">
+        <div className="text-center">
+          <h1 className="display-3 fw-bold lh-1 gradient-text3">About Me</h1>
+          <h5 className="text-light">
+            <span className="px-2">
+              <Sparkles color="#8254d9" />
+            </span>
+             Transforming ideas into digital experiences
+            <span className="px-2">
+              <Sparkles color="#8254d9" />
+            </span>
+          </h5>
+        </div>
       </section>
     </>
   );
